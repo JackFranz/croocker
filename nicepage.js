0ec506bfedc3fff9225fb57364113395dbf9bdfc
JackFranz <78769005+JackFranz@users.noreply.github.com>
2022-08-27 16:45:20 -0500

    Update nicepage.js

--- nicepage.js
+++ nicepage.js
@@ -1,11 +1,7 @@
  //HERO THE CODE OF THE SCREEN ORIENTATION
  function lock (orientation) {
     // (A1) GO INTO FULL SCREEN FIRST
-    let de = document.documentElement;
-    if (de.requestFullscreen) { de.requestFullscreen(); }
-    else if (de.mozRequestFullScreen) { de.mozRequestFullScreen(); }
-    else if (de.webkitRequestFullscreen) { de.webkitRequestFullscreen(); }
-    else if (de.msRequestFullscreen) { de.msRequestFullscreen(); }
+    
 
     // (A2) THEN LOCK ORIENTATION
     screen.orientation.lock(orientation);
